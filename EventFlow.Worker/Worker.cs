53eb34910e86150faba0a664c556969cb83d9e92
Aman Pal <palaman2626@gmail.com>
2026-09-09 17:55:05 +0530

    testing: worker able to save data to the database

--- EventFlow.Worker/Worker.cs
+++ EventFlow.Worker/Worker.cs
@@ -1,10 +1,32 @@
-namespace EventFlow.Worker;
 using EventFlow.Infrastructure.Data;
+using EventFlow.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventFlow.Worker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IServiceScopeFactory _scopeFactory;
+    EventData[] events = new EventData[]
+     {
+        new EventData
+        {
+            Type = "Order Created",
+            Payload = "order has been created proceed further!"
+        },
+        new EventData
+        {
+            Type = "Notification Sent",
+            Payload = "notification has been sent."
+        },
+        new EventData
+        {
+            Type = "Payment Successful",
+            Payload = "payment has been made successfully."
+        }
+     };
+    Random random = new Random();
 
     public Worker(ILogger<Worker> logger,IServiceScopeFactory scopeFactory){
         _logger = logger;
@@ -23,9 +45,27 @@ public class Worker : BackgroundService
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
 
+            int randomIndex = random.Next(events.Length);
+            EventData randomEvent = events[randomIndex];
 
+            await db.Events.AddAsync(new Event
+            {
+                Id = Guid.NewGuid(),
+                Type = randomEvent.Type,
+                CurrStatus = Status.Pending,
+                CreatedAt = DateTime.UtcNow,
+                Payload = randomEvent.Payload
+            });
 
-            await Task.Delay(1000, stoppingToken);
+            await db.SaveChangesAsync(stoppingToken);
+
+            await Task.Delay(1000,stoppingToken);
         }
     }
+}
+
+public class EventData
+{
+    public string Type { get; set; }
+    public string Payload { get; set; }
 }
