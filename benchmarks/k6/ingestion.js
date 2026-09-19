4e60f4205671b1149b12495df37f16e0eb02e3da
Aman Pal <palaman2626@gmail.com>
2026-09-19 11:40:48 +0530

    add : k6 tests for ingestion service

---
+++ benchmarks/k6/ingestion.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus:10,
+    duration:'30s',
+}
+
+export default function (){
+    const payload = JSON.stringify({
+        Type:'OrderCreated',
+        Payload:{
+            orderId:`ORD-${__VU}-${__ITER}`,
+            amount:2480,
+            currency:'INR'
+        }
+    });
+
+    const response = http.post(
+        'http://host.docker.internal:5000/api/events',
+        payload,
+        {
+            headers:{
+                'Content-Type':'application/json',
+            }
+        }
+    );
+
+    check(response,{
+        'event accepted':(r) => r.status === 200,
+    })
+}
